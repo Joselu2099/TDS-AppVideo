6b945bd698d9a6c7fb943151a9b4f8f8cea3e655
Jose Luis Sánchez Carrasco <38717344+Joselu2099@users.noreply.github.com>
2021-09-24 14:41:39 +0200

    AppVideo 0.1
    Creando la ventana de Login y programada casi toda la lógica de negocio (modelo)

---
+++ AppVideo/src/main/java/model/Filter.java
@@ -0,0 +1,5 @@
+package model;
+
+public interface Filter {
+	public void eliminarVideos();
+}
---
+++ AppVideo/src/main/java/model/FilterMyLists.java
@@ -0,0 +1,11 @@
+package model;
+
+public class FilterMyLists implements Filter {
+
+	@Override
+	public void eliminarVideos() {
+		// TODO Auto-generated method stub
+
+	}
+
+}
