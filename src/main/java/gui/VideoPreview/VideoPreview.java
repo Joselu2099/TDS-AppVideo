a28aee43a53724281f8fcf0065e5c275ee3cd344
Menchen <huangmenchen@gmail.com>
2021-11-21 03:12:57 +0100

    WIP Multiline Title

--- src/main/java/gui/VideoPreview/VideoPreview.java
+++ src/main/java/gui/VideoPreview/VideoPreview.java
@@ -1,15 +1,14 @@
 package gui.VideoPreview;
 
+import gui.Util.MultilineLabel;
 import gui.Util.UIUtils;
 import gui.Util.VideoWebFactory;
 import model.Video;
 
-import javax.swing.JPanel;
+import javax.swing.*;
 import java.awt.*;
 import java.awt.event.MouseAdapter;
 import java.awt.event.MouseEvent;
-import javax.swing.JLabel;
-import javax.swing.SwingConstants;
 
 public class VideoPreview extends JPanel {
 
@@ -18,20 +17,28 @@ public class VideoPreview extends JPanel {
 	 */
 	Video video;
 	JLabel lblPreviewLabel = new JLabel("");
-	JLabel lblTitle = new JLabel("Title");
 	public VideoPreview(Video v,VideoPreviewCallback videoCallback) {
 		this.video = v;
 
 
 		setLayout(new BorderLayout(0, 0));
 		setMinimumSize(new Dimension(180,140));
+		setMinimumSize(new Dimension(200,140));
+		setMaximumSize(new Dimension(200,140));
 
+		// 120x90
+		Dimension thumbnailSize = new Dimension(120,90);
+		lblPreviewLabel.setSize(thumbnailSize);
+		lblPreviewLabel.setMaximumSize(thumbnailSize);
+		lblPreviewLabel.setMinimumSize(thumbnailSize);
+		lblPreviewLabel.setPreferredSize(thumbnailSize);
 		lblPreviewLabel.setIcon(VideoWebFactory.getInstance().getThumb(v.getUrl()));
 		lblPreviewLabel.setHorizontalAlignment(SwingConstants.CENTER);
-		add(lblPreviewLabel, BorderLayout.CENTER);
+		add(lblPreviewLabel, BorderLayout.NORTH);
+		lblPreviewLabel.setAlignmentX(JComponent.CENTER_ALIGNMENT);
 
-		lblTitle.setHorizontalAlignment(SwingConstants.CENTER);
-		lblTitle.setText(UIUtils.stringToHTML(v.getTitle()));
+		MultilineLabel lblTitle = new MultilineLabel(v.getTitle());
+		lblTitle.setAlignmentX(JComponent.CENTER_ALIGNMENT);
 		add(lblTitle, BorderLayout.SOUTH);
 
 
--- src/main/java/gui/VideoPreview/VideoPreviewListPanel.java
+++ src/main/java/gui/VideoPreview/VideoPreviewListPanel.java
@@ -19,22 +19,4 @@ public class VideoPreviewListPanel extends JPanel {
         setLayout(new FlowLayout());
         videoList.stream().map(v -> new VideoPreview(v, callback)).forEach(this::add);
     }
-
-
-    public static void main(String[] args) {
-        IntelliJTheme.setup(Launcher.class.getResourceAsStream("/themes/ArcPurple.theme.json"));
-//        IntelliJTheme.setup(Launcher.class.getResourceAsStream("/themes/DarkPurple.theme.json"));
-        EventQueue.invokeLater(() -> {
-            try {
-                String[] url = {
-                        "https://www.youtube.com/watch?v=56eIZKyhM6c",
-                };
-                List<Video> videoList = Arrays.stream(url).map(Video::new).collect(Collectors.toList());
-                VideoPreviewListPanel videoPreviewListPanel = new VideoPreviewListPanel(videoList,null);
-                videoPreviewListPanel.setVisible(true);
-            } catch (Exception e) {
-                e.printStackTrace();
-            }
-        });
-    }
 }
