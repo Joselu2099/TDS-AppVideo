7c8c0872ec4762a8196a366d7d5648a5d584cd4f
Menchen <huangmenchen@gmail.com>
2021-11-28 13:01:23 +0100

    Add scroll to homepanel

--- src/main/java/gui/HomePanel.java
+++ src/main/java/gui/HomePanel.java
@@ -1,12 +1,15 @@
 package gui;
 
+import com.formdev.flatlaf.IntelliJTheme;
 import gui.Util.SwapLayoutPanelWrapper;
 import gui.VideoPreview.VideoPreviewListPanel;
+import launcher.Launcher;
 import model.Video;
 import javax.swing.*;
 import org.jetbrains.annotations.NotNull;
 import controller.AppVideo;
 import java.awt.*;
+import java.util.Arrays;
 import java.util.List;
 import java.util.stream.Collectors;
 import java.awt.event.ActionListener;
@@ -53,9 +56,13 @@ public class HomePanel extends JPanel{
 			}
 		});
 		searchPanel.add(btnLoadVideos);
+		JScrollPane scrollPane = new JScrollPane(vidPanel.getPanel(),JScrollPane.VERTICAL_SCROLLBAR_ALWAYS,JScrollPane.HORIZONTAL_SCROLLBAR_NEVER);
+
+		showVideoPreview(repoList);
+		add(scrollPane,BorderLayout.CENTER);
 	}
 
-	public void showVideoPreview(List<Video> videoList) {
+	private void showVideoPreview(List<Video> videoList) {
 		currentList = videoList;
 		vidPanel.swap(new VideoPreviewListPanel(videoList,vid->{
 			VideoPlayerWindow player = new VideoPlayerWindow(vid);
@@ -73,6 +80,68 @@ public class HomePanel extends JPanel{
 	public void updateVideoList(@NotNull List<Video> videoList){
 		this.repoList = videoList;
 		showVideoPreview(videoList);
+	}
+
+	public static void main(String[] args) {
+		IntelliJTheme.setup(Launcher.class.getResourceAsStream("/themes/ArcPurple.theme.json"));
+//        IntelliJTheme.setup(Launcher.class.getResourceAsStream("/themes/DarkPurple.theme.json"));
+		EventQueue.invokeLater(() -> {
+			try {
+				String[] url = {
+						"https://www.youtube.com/watch?v=bxF-pQSzSUM",
+						"https://www.youtube.com/watch?v=XKfgdkcIUxw",
+						"https://www.youtube.com/watch?v=56eIZKyhM6c",
+						"https://www.youtube.com/watch?v=bxF-pQSzSUM",
+						"https://www.youtube.com/watch?v=XKfgdkcIUxw",
+						"https://www.youtube.com/watch?v=56eIZKyhM6c",
+						"https://www.youtube.com/watch?v=bxF-pQSzSUM",
+						"https://www.youtube.com/watch?v=XKfgdkcIUxw",
+						"https://www.youtube.com/watch?v=56eIZKyhM6c",
+						"https://www.youtube.com/watch?v=bxF-pQSzSUM",
+						"https://www.youtube.com/watch?v=XKfgdkcIUxw",
+						"https://www.youtube.com/watch?v=56eIZKyhM6c",
+						"https://www.youtube.com/watch?v=bxF-pQSzSUM",
+						"https://www.youtube.com/watch?v=XKfgdkcIUxw",
+						"https://www.youtube.com/watch?v=56eIZKyhM6c",
+						"https://www.youtube.com/watch?v=bxF-pQSzSUM",
+						"https://www.youtube.com/watch?v=XKfgdkcIUxw",
+						"https://www.youtube.com/watch?v=56eIZKyhM6c",
+						"https://www.youtube.com/watch?v=bxF-pQSzSUM",
+						"https://www.youtube.com/watch?v=XKfgdkcIUxw",
+						"https://www.youtube.com/watch?v=56eIZKyhM6c",
+						"https://www.youtube.com/watch?v=bxF-pQSzSUM",
+						"https://www.youtube.com/watch?v=XKfgdkcIUxw",
+						"https://www.youtube.com/watch?v=56eIZKyhM6c",
+						"https://www.youtube.com/watch?v=bxF-pQSzSUM",
+						"https://www.youtube.com/watch?v=XKfgdkcIUxw",
+						"https://www.youtube.com/watch?v=56eIZKyhM6c",
+						"https://www.youtube.com/watch?v=bxF-pQSzSUM",
+						"https://www.youtube.com/watch?v=XKfgdkcIUxw",
+						"https://www.youtube.com/watch?v=56eIZKyhM6c",
+						"https://www.youtube.com/watch?v=bxF-pQSzSUM",
+						"https://www.youtube.com/watch?v=XKfgdkcIUxw",
+						"https://www.youtube.com/watch?v=56eIZKyhM6c",
+						"https://www.youtube.com/watch?v=bxF-pQSzSUM",
+						"https://www.youtube.com/watch?v=XKfgdkcIUxw",
+						"https://www.youtube.com/watch?v=56eIZKyhM6c",
+				};
+				List<Video> videoList = Arrays.stream(url).map(Video::new).collect(Collectors.toList());
+				JFrame f = new JFrame();
+				HomePanel h = new HomePanel(f,videoList);
+				f.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+				f.setContentPane(h);
+				f.setBounds(0, 0, 800, 600);
+				f.setVisible(true);
+				String[] urlX = {
+						"https://www.youtube.com/watch?v=bxF-pQSzSUM",
+						"https://www.youtube.com/watch?v=56eIZKyhM6c",
+				};
+				List<Video> videoListX = Arrays.stream(urlX).map(Video::new).collect(Collectors.toList());
+//				h.updateVideoList(videoListX);
+			} catch (Exception e) {
+				e.printStackTrace();
+			}
+		});
 	}
 }
 
